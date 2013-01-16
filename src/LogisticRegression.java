3e9c6e4b453605efab4753d8affff81ea04e4420
Ruth King <R.A.King@warwick.ac.uk>
2013-01-16 19:04:48 +0000

    Added SMOTE & tidied code

--- src/LogisticRegression.java
+++ src/LogisticRegression.java
@@ -1,88 +1,57 @@
 import weka.classifiers.CostMatrix;
-import weka.classifiers.functions.SimpleLogistic;
+import weka.classifiers.Evaluation;
+import weka.classifiers.functions.Logistic;
 import weka.classifiers.meta.MetaCost;
-import weka.core.Attribute;
-import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.filters.Filter;
 import weka.filters.supervised.attribute.Discretize;
-import weka.filters.supervised.instance.Resample;
 import weka.filters.supervised.instance.SMOTE;
 
 public class LogisticRegression {
 
 	public static void main(String[] args) {
 		
-		//String filename = args[0];
+		int argsLength = args.length;
+		
+		if (argsLength < 1) {
+			System.out.println("Please specific a test data file");
+			System.exit(0);
+		}
+		String filename = args[0];
 
 		Instances trainingInstances = readInstances("trainingdata");
-		
-		Resample resample = new Resample();
-		try {
-			resample.setInputFormat(trainingInstances);
-			resample.setBiasToUniformClass(0.5);
-		} catch (Exception e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-
-		try {
-			trainingInstances = Filter.useFilter(trainingInstances, resample);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		Instances testInstances = readInstances(filename);
 
 		// Discretise the continuous attributes
 		Discretize discreteFilter = new Discretize();
-		SMOTE smote = new SMOTE();
 		try {
 			discreteFilter.setInputFormat(trainingInstances);
-		} catch (Exception e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-
-		try {
 			trainingInstances = Filter.useFilter(trainingInstances, discreteFilter);
+			testInstances = Filter.useFilter(testInstances, discreteFilter);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		/*try {
-			smote.setInputFormat(instances);
-			smote.setRandomSeed(10);
-		} catch (Exception e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-
-		try {
-			instances = Filter.useFilter(instances, smote);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}		*/
-
-
-		System.out.println("finished filtering");
-
-		SimpleLogistic logistic = new SimpleLogistic();
-		try {
-			logistic.buildClassifier(trainingInstances);
-			//costSense.buildClassifier(instances);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			System.out.println("Error with Discretize");
 			e.printStackTrace();
 		}
 		
+		SMOTE smote = new SMOTE();
+		try {
+			smote.setInputFormat(trainingInstances);
+			trainingInstances = Filter.useFilter(trainingInstances, smote);
+		} catch (Exception e) {
+			System.out.println("Error with SMOTE");
+			e.printStackTrace();
+		}
+
+		System.out.println("finished filtering");
+
+		Logistic logistic = new Logistic();
+		
 		CostMatrix costMatrix = new CostMatrix(2);
-		costMatrix.setElement(0, 0, 1);
-		costMatrix.setElement(0, 1, -0.1);
-		costMatrix.setElement(1, 0, -0.2);
-		costMatrix.setElement(1, 1, 1);
+		costMatrix.setElement(0, 0, 0);
+		costMatrix.setElement(0, 1, 1);
+		costMatrix.setElement(1, 0, 15);
+		costMatrix.setElement(1, 1, 0);
 		
 		MetaCost metaCost = new MetaCost();
 		metaCost.setClassifier(logistic);
@@ -90,47 +59,23 @@ public class LogisticRegression {
 		try {
 			metaCost.buildClassifier(trainingInstances);
 		} catch (Exception e1) {
-			// TODO Auto-generated catch block
+			System.out.println("Error building the MetaCost classifier");
 			e1.printStackTrace();
 		}
 
 		System.out.println("built the classifier");
 		
-		//Instances testInstances = readInstances("testdata");
-
-		int truePos = 0;
-		int falsePos = 0;
-		int trueNeg = 0;
-		int falseNeg = 0;
-
-		int numInstances = trainingInstances.numInstances();
-		for (int i = 0; i < numInstances; i++) {
-			try {
-				Instance thisInst = trainingInstances.instance(i);
-				double val = logistic.classifyInstance(thisInst);
-
-				Attribute actualAttr = thisInst.attribute(0);
-				double actualVal = thisInst.value(actualAttr);
-				if (val == 1.0 && actualVal == 1.0) {
-					truePos++;
-				} else if (val == 1.0 && actualVal == 0.0) {
-					falsePos++;
-				} else if (val == 0.0 && actualVal == 0.0) {
-					trueNeg++;
-				} else {
-					falseNeg++;
-				}
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		// Print out the results
+		Evaluation evaluation = null;
+		try {
+			evaluation = new Evaluation(trainingInstances);
+			evaluation.evaluateModel(metaCost, testInstances);
+			System.out.println(evaluation.toSummaryString());
+			System.out.println(evaluation.toMatrixString());
+			System.out.println(evaluation.toClassDetailsString());
+		} catch (Exception e) {
+			System.out.println("Error outputting evaluation");
 		}
-
-		System.out.println("correct ones : " + truePos + " correct zeros : " + trueNeg + 
-				" false neg : " + falsePos + " false pos : " + falseNeg);
-		
-		System.out.println(truePos + "   " + falseNeg);
-		System.out.println(falsePos + "    " + trueNeg);
 	}
 	
 	private static Instances readInstances(String file) {
